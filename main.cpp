edab212fb6d2e89cfac8b702f8347fb2258dc6a8
kramber1024 <kramber268@gmail.com>
2024-05-04 23:27:51 +0300

    feat: add timers in main

--- main.cpp
+++ main.cpp
@@ -2,19 +2,59 @@
 
 using namespace std;
 
-int main()
-{
-	MyStack stack;
 
-    for (int i = 1; i < 6; i++)
+void simple_test()
+{
+    int n;
+    cout << "Please, enter number of elements n = ";
+    cin >> n;
+
+    MyStack stack;
+
+    for (int i = 0; i < n; i++)
     {
-        stack.Push(i);
+        int x = rand() % 101;
+        stack.Push(x);
     }
+
     cout << "before: ";
     stack.ShowAll();
+    cout << boolalpha << "sorted: " << stack.Sorted() << endl;
 
-    stack.Set(3, 99);
-    
+    stack.Sort(stack.Length());
+
     cout << "after: ";
     stack.ShowAll();
+    cout << boolalpha << "sorted: " << stack.Sorted() << endl;
+}
+
+void full_test()
+{
+    unsigned long long int t_s, t_f;
+    int j, i;
+    for (i = 1; i < 11; i++)
+    {
+        MyStack stack;
+
+        for (j = 0; j < i * 100; j++)
+        {
+            stack.Push(rand() % 100001);
+        }
+
+        t_s = GetTickCount64();
+        stack.Sort(stack.Length());
+        t_f = GetTickCount64();
+
+        cout << "sort #" << i << endl;
+        cout << "number of elements " << i * 100 << endl;
+        cout << "sort time (ms) " << t_f - t_s << endl;
+        cout << "number of operations: " << "NOT_READY" << endl;
+        cout << endl;
+    }
+}
+
+int main()
+{
+    simple_test();
+    full_test();
 }
--- main.h
+++ main.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
 
 #include <iostream>
+#include "windows.h"
 #include "mystack.h"
