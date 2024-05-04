6e477eb88a06f336ef06048a178f1803acaa67f6
kramber <kramber268@gmail.com>
2024-05-04 19:56:38 +0300

    feat: add set method

--- mystack.cpp
+++ mystack.cpp
@@ -1,21 +1,41 @@
 #include "mystack.h"
 
+using namespace std;
+
 
 int MyStack::Get(int pos)
 {
     Stack temporaryStack;
 
-    for (int i = 0; i < pos; i++)
+    for (int i = 0; i < pos - 1; i++)
     {
         temporaryStack.Push(Pop());
     }
 
     int result = ShowHead();
 
-    for (int i = 0; i < pos; i++)
+    for (int i = 0; i < pos - 1; i++)
     {
         Push(temporaryStack.Pop());
     }
 
     return result;
+}
+
+void MyStack::Set(int pos, int num)
+{
+    Stack temporaryStack;
+
+    for (int i = 0; i < pos - 1; i++)
+    {
+        temporaryStack.Push(Pop());
+    }
+
+    Pop();
+    Push(num);
+
+    for (int i = 0; i < pos - 1; i++)
+    {
+        Push(temporaryStack.Pop());
+    }
 }
