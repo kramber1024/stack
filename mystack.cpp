de4633261989b5966991d5d6ac2402807b08c3ec
kramber1024 <kramber268@gmail.com>
2024-05-04 22:23:32 +0300

    feat: finish MyStack class

--- mystack.cpp
+++ mystack.cpp
@@ -7,14 +7,14 @@ int MyStack::Get(int pos)
 {
     Stack temporaryStack;
 
-    for (int i = 0; i < pos - 1; i++)
+    for (int i = 0; i < pos; i++)
     {
         temporaryStack.Push(Pop());
     }
 
     int result = ShowHead();
 
-    for (int i = 0; i < pos - 1; i++)
+    for (int i = 0; i < pos; i++)
     {
         Push(temporaryStack.Pop());
     }
@@ -26,7 +26,7 @@ void MyStack::Set(int pos, int num)
 {
     Stack temporaryStack;
 
-    for (int i = 0; i < pos - 1; i++)
+    for (int i = 0; i < pos; i++)
     {
         temporaryStack.Push(Pop());
     }
@@ -34,8 +34,27 @@ void MyStack::Set(int pos, int num)
     Pop();
     Push(num);
 
-    for (int i = 0; i < pos - 1; i++)
+    for (int i = 0; i < pos; i++)
     {
         Push(temporaryStack.Pop());
+    }
+}
+
+void MyStack::Sort(int n)
+{
+    for (int gap = n / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < n; i += 1)
+        {
+            int j;
+            int temp = Get(i);
+
+            for (j = i; j >= gap && Get(j - gap) > temp; j -= gap)
+            {
+                Set(j, Get(j - gap));
+            }                
+
+            Set(j, temp);
+        }
     }
 }
--- mystack.h
+++ mystack.h
@@ -8,5 +8,5 @@ class MyStack : public Stack {
 public:
     int Get(int pos);
     void Set(int pos, int num);
-    void Sort();
+    void Sort(int n);
 };
