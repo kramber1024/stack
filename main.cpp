d27f93b5d74a8e8751a0e9de00e869e6bba13cff
kramber <kramber268@gmail.com>
2024-05-04 10:08:23 +0300

    feat: init main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#include "main.h"
+
+using namespace std;
+
+int main()
+{
+	MyStack stack;
+
+    for (int i = 1; i < 101; i++)
+    {
+        stack.Push(i);
+    }
+    cout << stack.Length() << endl;
+    stack.ShowAll();
+    // cout << stack.Get(3) << endl;
+    // stack.ShowAll();
+}
