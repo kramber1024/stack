80f56ae491d435eec602000d76956cf46a09f85b
kramber1024 <kramber268@gmail.com>
2024-05-14 23:02:36 +0300

    refactor: move implementation of methods for templated classes into header files

--- main.h
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-
-#include <iostream>
-#include "windows.h"
-#include "mystack.h"
-#include "mystack.cpp"
--- mystack.h
+++ mystack.h
@@ -1,15 +1,67 @@
 #pragma once
 
 #include "stack.h"
-#include "stack.cpp"
 
-namespace structures
+template <typename T> class MyStack : public Stack<T>
 {
-    template <typename T> class MyStack : public Stack<T>
+    public:
+        T Get(int pos);
+        void Set(int pos, T num);
+        void Sort(int n);
+};
+
+template <typename T> T MyStack<T>::Get(int pos)
+{
+    Stack<T> temporaryStack;
+
+    for (int i = 0; i < pos; i++)
     {
-        public:
-            T Get(int pos);
-            void Set(int pos, T num);
-            void Sort(int n);
-    };
+        temporaryStack.Push(this->Pop());
+    }
+
+    T result = this->ShowHead();
+
+    for (int i = 0; i < pos; i++)
+    {
+        this->Push(temporaryStack.Pop());
+    }
+
+    return result;
+}
+
+template <typename T> void MyStack<T>::Set(int pos, T num)
+{
+    Stack<T> temporaryStack;
+
+    for (int i = 0; i < pos; i++)
+    {
+        temporaryStack.Push(this->Pop());
+    }
+
+    this->Pop();
+    this->Push(num);
+
+    for (int i = 0; i < pos; i++)
+    {
+        this->Push(temporaryStack.Pop());
+    }
+}
+
+template <typename T> void MyStack<T>::Sort(int n)
+{
+    for (int gap = n / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < n; i += 1)
+        {
+            int j;
+            T temp = Get(i);
+
+            for (j = i; j >= gap && Get(j - gap) > temp; j -= gap)
+            {
+                Set(j, Get(j - gap));
+            }                
+
+            Set(j, temp);
+        }
+    }
 }
