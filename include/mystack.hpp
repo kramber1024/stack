6d9d8eca33da9ba7c5dd2f2071c7c7f066ab74e8
kramber1024 <kramber268@gmail.com>
2024-05-30 23:25:19 +0300

    feat: add documentation

--- include/mystack.hpp
+++ include/mystack.hpp
@@ -1,144 +1,151 @@
 #pragma once
 
-#include "stack.h"
+#include "stack.hpp"
 
 /**
- * @brief Стек с расширенными возможностями.
- * 
- * Унаследован от Stack.
- * 
- * @tparam T Тип данных в стеке.
+ * @class MyStack
+ * @brief A template class representing a Stack with extended functionality.
+ *
+ * This class provides the functionality manipulate elements in a stack
+ * with setters and getter methods, and a sorting method.
+ *
+ * @tparam T The type of elements stored in the stack.
  */
 template <typename T> class MyStack : public Stack<T>
 {
-    public:
-        T Get(int pos);           // 35n + 26
-        void Set(int pos, T num); // 35n + 34
-        T& operator[](int n);     // 35n + 25
-        void Sort(int n);         // n^2
+public:
+	T Get(int pos);		      // 35n + 26
+	void Set(int pos, T num); // 35n + 34
+	T& operator[](int n);	  // 35n + 25
+	void Sort(int n);		  // n^2
 };
 
 /**
- * @brief Возвращает значение из стека по позиции, считая с верха стека.
+ * @brief Retrieves the element at the specified position in the stack.
  * 
- * Чтобы получить значение по позиции, основной стек "разбирается" в вспомогательный.
- * После получения значения стек "собирается" обратно из вспомогательного.
- * `Метод небезопасен, так как отсутствуют проверки на входной параметр`.
+ * This method retrieves the element at the specified position in the stack.
+ * The position is zero-based, so the first element is at position 0, the second
+ * element is at position 1, and so on.
  * 
- * @tparam T Тип данных в стеке.
- * @param pos Позиция, с которой будет читаться значение.
- * @return T Значение, находящиеся на позиции.
+ * @tparam T The type of elements stored in the stack.
+ * @param pos The position of the element to retrieve.
+ * @return The element at the specified position.
  */
 template <typename T> T MyStack<T>::Get(int pos) // 35n + 26
 {
-    Stack<T> temporaryStack;                     this->N_op++;
+	Stack<T> temporaryStack;					 this->N_op++;
 
-    this->N_op+=3;
-    for (int i = 0; i < pos; i++)
-    {
-        this->N_op+=2;
-        temporaryStack.Push(this->Pop());
-    }
+	this->N_op+=3;
+	for (int i = 0; i < pos; i++)
+	{
+		this->N_op+=2;
+		temporaryStack.Push(this->Pop());
+	}
 
-    T result = this->ShowHead();                 this->N_op+=2;
+	T result = this->ShowHead();				 this->N_op+=2;
 
-    this->N_op+=3;
-    for (int i = 0; i < pos; i++)
-    {
-        this->N_op+=2;
-        this->Push(temporaryStack.Pop());
-    }
+	this->N_op+=3;
+	for (int i = 0; i < pos; i++)
+	{
+		this->N_op+=2;
+		this->Push(temporaryStack.Pop());
+	}
 
-    this->N_op+=temporaryStack.N_op+1;
-    return result;
+	this->N_op+=temporaryStack.N_op+1;
+	return result;
 }
 
 /**
- * @brief Заменяет значение в стека по позиции, считая с верха стека.
+ * @brief Replaces the value in the stack at the given position, counting from the top of the stack.
  * 
- * Чтобы изменить значение по позиции, основной стек "разбирается" в вспомогательный.
- * После изменения значения стек "собирается" обратно из вспомогательного.
- * `Метод небезопасен, так как отсутствуют проверки на входной параметр`.
+ * To change the value at the specified position, the main stack is disassembled into a temporary stack.
+ * After changing the value, the stack is reassembled from the temporary stack.
+ * `This method is unsafe as it lacks input parameter checks`.
  * 
- * @tparam T Тип данных в стеке.
- * @param pos Позиция, на которой будет изменено значение.
- * @param num Новое значение.
+ * @tparam T The type of elements stored in the stack.
+ * @param pos The position at which the value will be changed.
+ * @param num The new value.
  */
 template <typename T> void MyStack<T>::Set(int pos, T num) // 35n + 34
 {
-    Stack<T> temporaryStack;                               this->N_op++;
+	Stack<T> temporaryStack;							   this->N_op++;
 
-    this->N_op+=3;
-    for (int i = 0; i < pos; i++)
-    {
-        this->N_op+=2;
-        temporaryStack.Push(this->Pop());
-    }
+	this->N_op+=3;
+	for (int i = 0; i < pos; i++)
+	{
+		this->N_op+=2;
+		temporaryStack.Push(this->Pop());
+	}
 
-    this->Pop();
-    this->Push(num);
+	this->Pop();
+	this->Push(num);
 
-    this->N_op+=3;
-    for (int i = 0; i < pos; i++)
-    {
-        this->N_op+=2;
-        this->Push(temporaryStack.Pop());
-    }
-    this->N_op+=temporaryStack.N_op;
-}
-
-template <typename T> T& MyStack<T>::operator[](int n) // 35n + 25
-{
-    Stack<T> temporaryStack;                           this->N_op+=2;
-
-    this->N_op+=3;
-    for (int i = 0; i < n; i++)
-    {
-        this->N_op+=2;
-        temporaryStack.Push(this->Pop());
-    }
-
-    T* valueAddress = this->GetHead();                 this->N_op+=2;
-
-    this->N_op+=3;
-    for (int i = 0; i < n; i++)
-    {
-        this->N_op+=2;
-        this->Push(temporaryStack.Pop());
-    }
-
-    this->N_op+=temporaryStack.N_op+2;
-    return *valueAddress;
+	this->N_op+=3;
+	for (int i = 0; i < pos; i++)
+	{
+		this->N_op+=2;
+		this->Push(temporaryStack.Pop());
+	}
+	this->N_op+=temporaryStack.N_op;
 }
 
 /**
- * @brief Сортирует значения в стеке алгоритмом Шелла.
- * 
- * После сортировки самые большие значения будут внизу стека: `1 2 3`.
- * 
- * @tparam T Тип данных в стеке.
- * @param n Длина стека. Необходим для асимптотической оценка алгоритма.
+ * @brief Overloaded subscript operator to access elements in the stack.
+ * @param n The index of the element to access.
+ * @return A reference to the element at the specified index.
  */
-template <typename T> void MyStack<T>::Sort(int n) // n^2                  
+template <typename T> T& MyStack<T>::operator[](int n) // 35n + 25
 {
-    this->N_op+=3;
-    for (int gap = n / 2; gap > 0; gap /= 2)
-    {
-        this->N_op+=5;
-        for (int i = gap; i < n; i += 1)
-        {
-            this->N_op+=2;
-            int j;                                                 this->N_op++;
-            T temp = Get(i);                                       this->N_op+=2;
+	Stack<T> temporaryStack;						   this->N_op+=2;
 
-            this->N_op+=5;
-            for (j = i; j >= gap && Get(j - gap) > temp; j -= gap)
-            {
-                this->N_op+=5;
-                Set(j, Get(j - gap));
-            }                
+	this->N_op+=3;
+	for (int i = 0; i < n; i++)
+	{
+		this->N_op+=2;
+		temporaryStack.Push(this->Pop());
+	}
 
-            Set(j, temp);
-        }
-    }
+	T* valueAddress = this->GetHead();				    this->N_op+=2;
+
+	this->N_op+=3;
+	for (int i = 0; i < n; i++)
+	{
+		this->N_op+=2;
+		this->Push(temporaryStack.Pop());
+	}
+
+	this->N_op+=temporaryStack.N_op+2;
+	return *valueAddress;
+}
+
+/**
+ * @brief Sorts the elements in the stack using the Shell sort algorithm.
+ * 
+ * The Shell sort algorithm is used to sort the elements in the stack in ascending order.
+ * It has a time complexity of O(n^2).
+ * 
+ * @param n The number of elements in the stack.
+ */
+template <typename T> void MyStack<T>::Sort(int n) // n^2				  
+{
+	this->N_op+=3;
+	for (int gap = n / 2; gap > 0; gap /= 2)
+	{
+		this->N_op+=5;
+		for (int i = gap; i < n; i += 1)
+		{
+			this->N_op+=2;
+			int j;								   this->N_op++;
+			T temp = Get(i);					   this->N_op+=2;
+
+			this->N_op+=5;
+			for (j = i; j >= gap && Get(j - gap) > temp; j -= gap)
+			{
+				this->N_op+=5;
+				Set(j, Get(j - gap));
+			}				
+
+			Set(j, temp);
+		}
+	}
 }
